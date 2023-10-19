28445567393095a9698a6e64b413d843cb836c7a
Elio <elio.sanfratello@gmail.com>
2023-10-19 17:41:42 +0200

    Ci vediamo domani

--- objectmethods.js
+++
@@ -1,6 +0,0 @@
-let person = { firstName: "Mario", lastName: "Rossi", age: 25 }
-let keys = Object.keys(person)
-
-for (el of keys) {
-    console.log(el + ": " + person[el]) 
-} 
---
+++ optionalchaining.js
@@ -0,0 +1,6 @@
+const order = {};
+
+if (order && order.customer && order.customer.address && !order.customer.address.city) {
+  console.log('City is required');
+}
+console.log(order)
