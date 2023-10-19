ae1c2b3f8861c67e40c10f3fcafc1e5cbd987a01
Elio <elio.sanfratello@gmail.com>
2023-10-19 13:04:14 +0200

    correzione

--- objectcopying.js
+++ objectcopying.js
@@ -1,14 +1,15 @@
 const person1 = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-  };
-  
-  const person2 = Object.assign({}, person1);
-  person2.firstName = "Simon"
-// In questo modo siamo andati a cambiare solamente il VALUE del firstName e non la Reference.
-// Se avessimo cambiato il valore di firstName in person2 ricopiando per intero l'oggetto, avremmo cambiato anche il valore di person1
-  
-  console.log(person1);
-  console.log(person2);
-  
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+};
+
+const person2 = person1;
+
+person2.firstName = "Simon"
+
+//Nonostante la presenza di due variabili (person1, person2), il riferimento è fatto allo stesso oggetto (person1).
+//In questo modo, cambiando il contenuto di una delle due variabili, automaticamente andrò a cambiare l'oggetto a cui fanno riferimento
+// Esempio: dire person1.firstname="Elio" === person2.firstname="Elio"
+console.log(person1);
+console.log(person2);
