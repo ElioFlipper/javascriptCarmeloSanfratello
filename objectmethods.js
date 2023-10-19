f0daa40225420f0b626c1e5bdb93870adfa4546f
Elio <elio.sanfratello@gmail.com>
2023-10-19 12:53:15 +0200

    Esercizio da completare

--- objectmethods.js
+++ objectmethods.js
@@ -1,5 +1,4 @@
 let person = { firstName: "Mario", lastName: "Rossi", age: 25 }
 let keys = Object.keys(person)
 let values= Object.values(person)
-console.log(keys)
-console.log(values)
+console.log(keys=values)
