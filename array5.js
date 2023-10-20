182a64c8c10cca9aab8d840d2b23a0a747e0af77
Elio <elio.sanfratello@gmail.com>
2023-10-20 11:00:20 +0200

    array 5

---
+++ array5.js
@@ -0,0 +1,21 @@
+function sortPeopleByAge(people) {
+  
+}
+
+const people = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+let sorted = people.sort((a, b) => a.age - b.age)
+// const sortingByAge = sortPeopleByAge(people)
+// console.log(sortingByAge)
+console.log(sorted)
